91477b3d0a718a17987e243532fac4b520c5d582
iceefast <samuel.cristian17062004@gmail.com>
2023-08-21 12:13:40 -0300

    fechando o modal com esc, validacao de dados, modularizando a caixa de erro.

--- js/main.js
+++ js/main.js
@@ -1,35 +1,45 @@
+import { Modal } from "./modal.js";
+import { AlertError } from "./alert-error.js";
+
 const form = document.querySelector("form");
 const inputWeight = document.querySelector("#inputWeight");
 const inputHeight = document.querySelector("#inputHeight");
-
-const Modal = {
-  wrapper: document.querySelector(".modal-wrapper"),
-  message: document.querySelector(".modal .title span"),
-  buttonClose: document.querySelector(".modal button.close"),
-  open() {
-    Modal.wrapper.classList.add("open");
-  },
-  close() {
-    Modal.wrapper.classList.remove("open");
-  },
-};
 
 form.onsubmit = (event) => {
   event.preventDefault();
 
   const weight = inputWeight.value;
   const height = inputHeight.value;
+
+  const showAllertError = notNumber(weight) || notNumber(height);
+
+  if (showAllertError) {
+    AlertError.open();
+    return;
+  }
+  AlertError.close();
+
   const result = imc(weight, height);
   const message = `Seu IMC e de ${result}`;
 
   Modal.message.innerText = message;
-
   Modal.open();
-  console.log(result);
 };
+
+function notNumber(value) {
+  return;
+}
 
 Modal.buttonClose.onclick = () => Modal.close();
 
 function imc(weight, height) {
   return (weight / (height / 100) ** 2).toFixed(2);
+}
+
+window.addEventListener("keydown", handleEscClick);
+
+function handleEscClick(event) {
+  if (event.key == "Escape") {
+    Modal.close();
+  }
 }
